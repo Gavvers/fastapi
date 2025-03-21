14c1f5f6b4bdc193f27cde3ac92061fa6cebddae
Kenneth M. Glassey <awereal@gmail.com>
2025-03-20 19:01:53 -0500

    Implement tests to prove raise_from_deps.

--- tests/test_dependency_exception.py
+++ tests/test_dependency_exception.py
@@ -1,4 +1,7 @@
+from typing_extensions import Annotated
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.dependencies.requesterrors import RequestErrors
+from fastapi.param_functions import Query
 from fastapi.testclient import TestClient
 
 app = FastAPI()
@@ -32,29 +35,120 @@ async def put_user_async(dep: dict = Depends(dep_test_exception_async)):
     return {"message": "OK"}
 
 
+@app.get("/users-no-error")
+def get_user_no_error(
+    caught_validation_errors: RequestErrors,
+    dep: Annotated[int, Query()] = None,
+):
+    return {"message": "OK", "dep": dep, "errors": caught_validation_errors.errors}
+
+
+@app.get("/users-error")
+def get_user_error(dep: Annotated[int, Query()]):
+    return {"message": "OK"}
+
+
+@app.get("/users-no-error-async")
+async def get_user_no_error_async(
+    caught_validation_errors: RequestErrors, dep: Annotated[int, Query()] = None
+):
+    return {"message": "OK", "dep": dep, "errors": caught_validation_errors.errors}
+
+
+@app.get("/users-error-async")
+async def get_user_error_async(dep: Annotated[int, Query()]):
+    return {"message": "OK"}
+
+
 client = TestClient(app)
 
 
-def test_dependency_exception_continue_operation():
+def test_raise_no_deps_active_continue_operation():
     response = client.get("/users-no-raise")
     assert response.status_code == 200, response.text
     assert response.json() == {"message": "OK", "dep": None}
 
 
-def test_async_dependency_exception_continue_operation():
+def test_async_raise_no_deps_active_continue_operation():
     # TODO: Implement configuration to enable this test to pass
     response = client.get("/users-no-raise-async")
     assert response.status_code == 200, response.text
     assert response.json() == {"message": "OK", "dep": None}
 
 
-def test_dependency_exception_short_circuits():
+def test_raise_no_deps_inactive_short_circuits():
     response = client.get("/users")
     assert response.status_code == 400, response.text
     assert response.json() == {"detail": "Invalid request"}
 
 
-def test_async_dependency_exception_short_circuits():
+def test_async_raise_no_deps_inactive_short_circuits():
     response = client.get("/users-async")
     assert response.status_code == 400, response.text
     assert response.json() == {"detail": "Invalid request"}
+
+nonsense_value = "dfkjnkldjfn"
+
+def test_requesterrors_continue_operation():
+    response = client.get(f"/users-no-error?dep={nonsense_value}")
+    assert response.status_code == 200, response.text
+    assert response.json() == {
+        "message": "OK",
+        "dep": None,
+        "errors": [
+            {
+                "input": nonsense_value,
+                "loc": ["query", "dep"],
+                "msg": "Input should be a valid integer, unable to parse string as an integer",
+                "type": "int_parsing",
+            }
+        ],
+    }
+
+
+def test_async_requesterrors_continue_operation():
+    # TODO: Implement configuration to enable this test to pass
+    response = client.get(f"/users-no-error-async?dep={nonsense_value}")
+    assert response.status_code == 200, response.text
+    assert response.json() == {
+        "message": "OK",
+        "dep": None,
+        "errors": [
+            {
+                "input": nonsense_value,
+                "loc": ["query", "dep"],
+                "msg": "Input should be a valid integer, unable to parse string as an integer",
+                "type": "int_parsing",
+            }
+        ],
+    }
+
+
+def test_normal_short_circuits():
+    response = client.get(f"/users-error?dep={nonsense_value}")
+    assert response.status_code == 422, response.text
+    assert response.json() == {
+        "detail": [
+            {
+                "input": nonsense_value,
+                "loc": ["query", "dep"],
+                "msg": "Input should be a valid integer, unable to parse string as an integer",
+                "type": "int_parsing",
+            }
+        ]
+    }
+
+
+def test_async_normal_short_circuits():
+    response = client.get(f"/users-error-async?dep={nonsense_value}")
+    assert response.status_code == 422, response.text
+    assert response.json() == {
+        "detail": [
+            {
+                "input": nonsense_value,
+                "loc": ["query", "dep"],
+                "msg": "Input should be a valid integer, unable to parse string as an integer",
+                "type": "int_parsing",
+            }
+        ]
+    }
